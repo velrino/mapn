639adea9214931aa2eab3ef393f571512f850d2f
Velrino <denismagalhaesvelrino@gmail.com>
2017-02-07 23:06:17 -0200

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,42 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+
+document.addEventListener("deviceready", function () {
+  $cordovaPlugin.someFunction().then(success, error);
+}, false);
+
+angular.module('MAPN', ['ionic', 'ngCordova', 'ngCordovaOauth'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.controller('LoginwithFacebook',function($scope,$cordovaOauth){
+
+  $scope.LoginwithFacebook = function(){
+    console.log("clicked");
+      $cordovaOauth.facebook("1342045892482911", ["denismagalhaes2@outlook.com"]).then(function(result) {
+      console.log( result );
+      }, function(error) {
+      console.log( error );
+    });
+  };
+
+ });
